438d5ce3dcbfa665bc6f39e1cb8751d3dfdfc38a
“Peeyush <peeyushrampal31@gmail.com>
2025-05-16 21:18:53 +0530

    Multiplication

---
+++ Week 3/Multiplication.py
@@ -0,0 +1,21 @@
+"""
+Program: multiply two numbers
+--------------------
+This program asks the user for two
+integers and prints the value of the first number
+multiplied with the second
+"""
+
+def main():
+    print("This program multiplies two numbers.")
+    a=int(input("Enter first number: "))
+    b=int(input("Enter second number: "))
+    print(a*b)
+
+
+
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
